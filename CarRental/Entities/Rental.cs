4acdd9f0e556ca8291f1b2fff23cb71c7c5ed234
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-22 13:54:28 -0300

    Início do projeto. Criação da classe Rental sem os métodos ainda

---
+++ CarRental/Entities/Rental.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Entities
+{
+    class Rental
+    {
+        public string CarModel { get; set; }
+        public DateTime Pickup { get; set; }
+        public DateTime Return { get; set; }
+
+        public Rental()
+        {
+        }
+        public Rental(string carModel, DateTime pickup, DateTime _return)
+        {
+            CarModel = carModel;
+            Pickup = pickup;
+            Return = _return;
+        }
+
+
+    }
+}
