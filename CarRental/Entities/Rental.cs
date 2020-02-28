5ac985a198418a3d43838ad12ca9748558f6fb97
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-27 23:38:42 -0300

    Solução 'CarRental' feita por mim

--- CarRental/Entities/Rental.cs
+++ CarRental/Entities/Rental.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace CarRental.Entities
 {
     class Rental
-    {
+    {        
         public string CarModel { get; set; }
         public DateTime Pickup { get; set; }
         public DateTime Return { get; set; }
@@ -14,12 +15,39 @@ namespace CarRental.Entities
         {
         }
         public Rental(string carModel, DateTime pickup, DateTime _return)
-        {
+        {            
             CarModel = carModel;
             Pickup = pickup;
             Return = _return;
         }
+        public double RentalValue(double pricePerHour, double pricePerDay)
+        {
+            TimeSpan diference = Return.Subtract(Pickup);
 
-
+            if (diference.TotalHours <= 12)
+            {
+                double totalPrice = pricePerHour * Math.Ceiling(diference.TotalHours);
+                if (totalPrice <= 100.00) {
+                    return totalPrice + totalPrice * 0.2;
+                }
+                else
+                {
+                    return totalPrice + totalPrice * 0.15;
+                }
+            }
+            else
+            {
+                double totalPrice = pricePerDay * Math.Ceiling(diference.TotalDays);
+                if (totalPrice <= 100.00)
+                {
+                    return totalPrice + totalPrice * 0.2;
+                }
+                else
+                {
+                    return totalPrice + totalPrice * 0.15;
+                }
+            }
+                        
+        }
     }
 }
--- CarRental/Program.cs
+++ CarRental/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using CarRental.Entities;
 
 namespace CarRental
 {
@@ -6,7 +8,22 @@ namespace CarRental
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string carModel = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime pickup = DateTime.Parse(Console.ReadLine());
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime _return = DateTime.Parse(Console.ReadLine());
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Rental rent = new Rental(carModel, pickup, _return);
+
+            Console.WriteLine("INVOICE: ");
+            Console.Write("Total payment: " + rent.RentalValue(pricePerHour, pricePerDay).ToString("F2"));
+
         }
     }
 }
